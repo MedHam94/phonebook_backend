1d6cab314dba5b912da25411b3e30789d0f1a76a
MedHam94 <hammas.m.mh@gmail.com>
2024-02-07 22:11:32 +0100

    add mongoose validation minlength

--- model/person.js
+++ model/person.js
@@ -9,8 +9,16 @@ mongoose.connect(url).then((res) => {
 });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type:String,
+    minLength:3,
+    required: true
+  },    
+  number: {
+    type:String,
+    minLength:8,
+    required: true
+  }
 });
 
 personSchema.set("toJSON", {
